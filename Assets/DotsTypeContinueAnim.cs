a971aa49d8009f5d243ff5cf5786c774373206e2
breadnone <64100867+breadnone@users.noreply.github.com>
2021-02-06 10:45:56 +0700

    Add scripts

---
+++ Assets/DotsTypeContinueAnim.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+
+public class DotsTypeContinueAnim : MonoBehaviour
+{
+    public Text textDots;
+    WaitForSeconds waiting = new WaitForSeconds(0.2f);
+    void OnEnable()
+    {
+        StopAllCoroutines();
+        ReCallLoop();
+    }
+    protected IEnumerator TypingDdot()
+    {
+        yield return waiting;
+        if(textDots.text.Equals("..."))
+        {
+            textDots.text = "";
+        }
+
+        textDots.text = textDots.text + ".";
+        ReCallLoop();
+        yield break;
+      
+    }
+    protected void ReCallLoop()
+    {
+        StartCoroutine(TypingDdot());
+    }
+    void OnDisable()
+    {
+        textDots.text = "";
+    }
+}
---
+++ Assets/continueAnime.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+
+public enum continueAnim
+{
+    CoinCircle,
+    Blink
+}
+public class continueAnime : MonoBehaviour
+{
+    public RectTransform continueIcon;
+    public GameObject textDots;
+    public Writer writer;
+    public continueAnim selectEffect = continueAnim.CoinCircle;
+    protected Vector3 cacheSiz;
+    protected int idd1;
+    public float durationOfEachCycle = 0.2f;
+
+    void OnEnable()
+    {
+        if(continueIcon != null)
+        {
+            textDots.SetActive(false);
+            cacheSiz = continueIcon.sizeDelta;
+
+            if(selectEffect == continueAnim.CoinCircle)
+            {
+                idd1 = LeanTween.scale(continueIcon, new Vector3(-1f, 1f, 1f), durationOfEachCycle).setLoopPingPong(-1).id;
+            }
+            if(selectEffect == continueAnim.Blink)
+            {
+                idd1 = LeanTween.alpha(continueIcon, 0f, durationOfEachCycle).setLoopPingPong(-1).id;
+            }
+        }
+    }
+    void OnDisable()
+    {
+        if(continueIcon != null)
+        {
+            textDots.SetActive(true);
+            LeanTween.cancel(idd1);
+            continueIcon.gameObject.transform.localScale = Vector3.one;
+            Image b = continueIcon.gameObject.GetComponent<Image>();
+
+            var tempColor = b.color;
+            tempColor.a = 1f;
+            b.color = tempColor;
+
+            continueIcon.sizeDelta = cacheSiz;            
+        }
+    }
+}
